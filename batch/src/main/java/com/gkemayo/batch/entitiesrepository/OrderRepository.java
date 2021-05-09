7a81a90d1d8ee55edd01aa361d57ef7dad118a70
cyrilvincent <contact@cyrilvincent.com>
2021-05-09 21:02:37 +0200

    Area

--- batch/src/main/java/com/gkemayo/batch/entitiesrepository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gkemayo.batch.entitiesrepository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.gkemayo.batch.entities.Order;
-
-public interface OrderRepository extends CrudRepository<Order, Long> {
-
-}
--- srcmaven/src/main/java/fr/aprr/formation/entities/IEntity.java
+++
@@ -1,9 +0,0 @@
-package fr.aprr.formation.entities;
-
-import jdk.jshell.spi.ExecutionControl;
-
-public interface IEntity {
-
-    public int getId();
-    public double getNetPrice() throws ExecutionControl.NotImplementedException;
-}
